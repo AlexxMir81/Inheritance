73e8f700bce9c7266edeab807071558632d243e1
Miro_up10@itstepru.onmicrosoft.com <Miro_up10@itstepru.onmicrosoft.com>
2022-01-29 19:32:00 +0300

    EndOfClassWork

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 class Human
 {
+//protected:
 	std::string last_name;
 	std::string first_name;
 	unsigned int age;
@@ -38,7 +39,7 @@ public:
 		set_last_name(last_name);
 		set_first_name(first_name);
 		set_age(age);
-		cout << "HKonstrucror:\t" << endl;
+		cout << "HConstrucror:\t" << this << endl;
 	}
 	~Human()
 	{
@@ -50,10 +51,161 @@ public:
 	}
 };
 
+class Student :public Human
+{
+	std::string speciality;
+	std::string group;
+	float rating;
+	float attendance;
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+	}
+	const std::string& get_group()const
+	{
+		return group;
+	}
+	float get_rating()const
+	{
+		return rating;
+	}
+	float get_attendance()const
+	{
+		return attendance;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_group(const std::string& group)
+	{
+		this->group = group;
+	}
+	void set_rating(float rating)
+	{
+		this->rating = rating;
+	}
+	void set_attendance(float attendance)
+	{
+		this->attendance = attendance;
+	}
+	Student
+	(
+		const std::string& last_name, const std::string& first_name, unsigned int age,
+		const std::string& speciality, const std::string& group, float rating, float attendance
+	):Human(last_name, first_name, age)
+	{
+		set_speciality(speciality);
+		set_group(group);
+		set_rating(rating);
+		set_attendance(attendance);
+		cout << "SConstructor:\t" << this << endl;
+	}
+	~Student()
+	{
+		cout << "SDestrucror:\t" << endl;
+	}
+	void print()const
+	{
+		Human::print();
+		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
+	}
+};
+
+class Teacher:public Human
+{
+	std::string speciality;
+	unsigned int experiance;
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+	}
+	unsigned int get_experiance()const
+	{
+		return experiance;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_experiance(unsigned int experiance)
+	{
+		this->experiance = experiance;
+	}
+
+
+	Teacher
+	(
+		const std::string& last_name, const std::string& first_name, unsigned int age,
+		const std::string& speciality, unsigned int experiance
+	):Human(last_name, first_name, age)
+	{
+		set_speciality(speciality);
+		set_experiance(experiance);
+		cout << "TConstrucror:\t" << this << endl;
+	}
+
+	~Teacher()
+	{
+		cout << "TDestrucror:\t" << endl;
+	}
+	void print()const
+	{
+		Human::print();
+		cout << speciality << " " << experiance << endl;
+	}
+};
+
+class Graduate : public Student
+{
+	std::string subject;
+public:
+	const std::string& get_subject()const
+	{
+		return subject;
+	}
+	void set_subject(const std::string& subject)
+	{
+		this->subject = subject;
+	}
+
+	Graduate
+	(
+		const std::string& last_name, const std::string& first_name, unsigned int age,
+		const std::string& speciality, const std::string& group, float rating, float attendance,
+		const std::string& subject
+	) :Student(last_name, first_name, age, speciality, group, rating, attendance)
+	{
+		set_subject(subject);
+		cout << "GConstructor\t" << this << endl;
+	}
+
+	~Graduate()
+	{
+		cout << "GDestructor\t" << this << endl;
+	}
+	void print()const
+	{
+		Student::print();
+		cout << subject << endl;
+	}
+};
+
 void main()
 {
 
 	setlocale(LC_ALL, "");
 	Human human("Тупенко", "Василий", 18);
 	human.print();
+
+	Student student("Иванов", "Иван", 20, "ООП", "SPD121", 10, 100);
+	student.print();
+
+	Teacher teacher("Петр","Петров", 50, "ООП", 20);
+	teacher.print();
+
+	Graduate graduate("Семенов", "Семен", 35, "ООП", "SPD121", 1, 100, "Делегирование конструкторов");
+	graduate.print();
 }
